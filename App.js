dc31a8d21d20ce93381d40be42b73658cdbc5765
mateus a <mateusaugustot17@gmail.com>
2023-03-06 22:32:54 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { View, Text,Image, Button, StyleSheet, TouchableOpacity } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component{
+  frases=['A vida trará coisas boas se tiver paciência',
+  'Não há que ser forte. Há que ser flexível.',
+  'Siga os bons e aprenda com eles']
+  
+  constructor(props){
+    super(props)
+    this.state={
+      nome:"",
+      imagemAtual: require('./assets/biscoito.png')
+    }
+    this.mudarNome = this.mudarNome.bind(this)
+    this.trocarImagem = this.trocarImagem.bind(this)
+  }
+    
+  mudarNome(frases){
+    this.setState({
+      nome:frases
+    })
+  }
+
+  trocarImagem(){
+    const novaImagem = this.state.imagemAtual === require('./assets/biscoito.png')
+      ? require('./assets/biscoitoQuebrado.png')
+      : require('./assets/biscoito.png');
+    this.setState({ imagemAtual: novaImagem });
+  }
+
+  render(){
+    return(
+      
+      <View style={{backgroundColor:"white", marginTop:40}}>
+      
+      <Image
+        
+          source={this.state.imagemAtual}
+          style={{width:400,height:450, textAlign:'center'}}>
+
+      </Image>
+
+        <Text style={{fontSize:30, textAlign:'center',color:"red", margin:20}}>
+         {this.state.nome}
+        </Text>
+        <TouchableOpacity style={styles.botao} onPress={()=>this.mudarNome(this.frases[Math.floor(Math.random() * this.frases.length)])}>
+        <Text style={styles.textoGeral}>Quebrar Biscoito</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.botao} onPress={this.trocarImagem}>
+          <Text style={styles.textoGeral}>Trocar Imagem</Text>
+        </TouchableOpacity>
+      </View>
+
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles =StyleSheet.create({
+  textoGeral:{
+    fontSize: 25,
+    color:"orange",
+    textAlign:'center'
   },
+  botao:{
+    backgroundColor:'white',
+    borderRadius:50,
+    height:50,
+    borderWidth:1,
+    borderColor:"#841584"
+  }
 });
+
+export default App;
